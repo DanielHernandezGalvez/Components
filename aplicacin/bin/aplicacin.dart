25a1549b1cbe59f2a87c7753cae55e3636147274
DanielHernandezGalvez <hernandezgalvezalejandro@gmail.com>
2022-10-29 22:30:58 -0500

    max min

--- aplicacin/bin/aplicacin.dart
+++ aplicacin/bin/aplicacin.dart
@@ -1,5 +1,38 @@
-import 'package:aplicacin/aplicacin.dart' as aplicacin;
-
+import 'dart:math';
 void main(List<String> arguments) {
-  print('Hello world: ${aplicacin.calculate()}!');
+  var lista = [
+    3,
+    45,
+    3,
+    1,
+    4,
+    23,
+    5,
+    24,
+  ];
+
+  var maximo = 0;
+  for (var numero in lista) {
+    if (maximo < numero) {
+      maximo = numero;
+    }
+    print('Ciclo: $maximo $numero');
+  }
+  print('El maximo es $maximo');
+
+  // Calcular el minimo
+
+  var minimo;
+
+  for (var numero in lista) {
+    if (minimo == null || minimo > numero) {
+      minimo = numero;
+    }
+    print('Ciclo: $minimo $numero');
+  }
+  print('El mínimo es $minimo');
+
+  print(lista.reduce(min));
+  print(lista.reduce(max));
+
 }
