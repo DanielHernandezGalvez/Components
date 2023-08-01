defa69126d468e48f81c1c0d1322302359df6100
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-07-31 22:54:30 -0600

    Reberberante

--- dartjuego/bin/galletas.dart
+++
@@ -1,9 +0,0 @@
-class personaje {
-  String? nombre;
-  int? nivel;
-
-  personaje(String nombreEntrada, int nivelEntrada) {
-    this.nombre = nombreEntrada;
-    this.nivel = nivelEntrada;
-  }
-}
