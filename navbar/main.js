d97da3ea031869f7eaab6689503996b20862ce53
unknown <henandezgalvezalejandro@gmail.com>
2023-10-09 17:44:29 -0600

    first component

---
+++ navbar/main.js
@@ -0,0 +1,7 @@
+// NAVBAR
+const navbarToggle = document.getElementById("navbarToggle");
+const navbarMenu = document.getElementById("navbarMenu");
+
+navbarToggle.addEventListener("click", () => {
+    navbarMenu.classList.toggle("active");
+});
