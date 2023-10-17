7388e204358a52194e677f9b2e142c7b1b669846
DanielHernandezGalvez <hernandezgalvezalejandro@gmail.com>
2023-10-17 12:59:56 -0600

    spotify web api

--- spotify-last-episode/main.js
+++ spotify-last-episode/main.js
@@ -1,29 +1,32 @@
-const SpotifyClient = require("spotify-web-api-node");
+// npm install spotify-web-api-node
 
 const clientId = "336910e6331f466ab460dff69bb94358";
 const clientSecret = "70565bc0dcf647538ab3c943a9f09ecc";
 
-const iframe = document.querySelector("iframe");
-const showId = iframe.dataset.showId;
+// Replace this with your show ID
+const showId = "35LWOWQ76ENHISgGpOyXE0"; // Debes reemplazar esto con el ID de tu programa
 
 const getLatestEpisode = async () => {
-    const client = new SpotifyClient({
-        clientId,
-        clientSecret,
-      });
-  const token = await client.getAccessToken();
-
-  const url = `https://api.spotify.com/v1/shows/${showId}/episodes/latest`;
-  const response = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+  // Create a new Spotify Web API client
+  const client = new SpotifyWebApi({
+    clientId,
+    clientSecret,
   });
-  const data = await response.json();
 
-  return data.episode;
+  // Authenticate and get an access token
+  const data = await client.clientCredentialsGrant();
+  const token = data.body.access_token;
+  client.setAccessToken(token);
+
+  // Use the client to get the latest episode of your show
+  const response = await client.getShowEpisodes(showId, { limit: 1, offset: 0 });
+  const latestEpisode = response.body.items[0];
+
+  return latestEpisode;
 };
 
 getLatestEpisode().then((episode) => {
-  iframe.src = `https://open.spotify.com/embed/episode/${episode.id}?si=${episode.uri}`;
+  // Update the iframe src with the latest episode's URI
+  const iframe = document.querySelector("iframe");
+  iframe.src = `https://open.spotify.com/embed/episode/${episode.id}`;
 });
